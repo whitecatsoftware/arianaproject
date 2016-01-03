ace0bd80d678bced90b63fd90954baa628008930
whitecatsoftware <semargl63@gmail.com>
2016-01-04 03:59:36 +0400

    Basic core classes and packages

---
+++ src/com/whitecatsoftware/arianaproject/core/DBService.java
@@ -0,0 +1,7 @@
+package com.whitecatsoftware.arianaproject.core;
+
+/**
+ * Created by Le on 04.01.2016.
+ */
+public class DBService {
+}
---
+++ src/com/whitecatsoftware/arianaproject/core/EntryPoint.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Le on 04.01.2016.
+ */
+
+package com.whitecatsoftware.arianaproject.core;
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+    }
+
+}
